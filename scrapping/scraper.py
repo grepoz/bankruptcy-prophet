9100ad99424d9aa83724813c4c7856b98f72e1ca
grepoz <grzegorz.pozorski@gmail.com>
2023-07-28 23:22:56 +0200

    develop scraper & add simple gurufocus scraper

--- scrapping/scraper.py
+++ scrapping/scraper.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_altman_z_score(ticker):
     url = f"https://www.gurufocus.com/stock/{ticker}/summary?search="
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find the Altman Z-score in the page
     altman_z_score = None
-    for div in soup.find_all('div', {'class': 'data_box'}):
+    for div in soup.find_all('div', {'class': 'grid-stack-item-content'}):
+
         if 'Altman Z-Score' in div.text:
             altman_z_score = div.find('span', {'class': 'data_box_value'}).text
             break
@@ -18,5 +19,5 @@ def get_altman_z_score(ticker):
     else:
         print(f"Altman Z-Score for {ticker} is {altman_z_score}")
 
-# Test the function
+
 get_altman_z_score('MSFT')
