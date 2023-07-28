2dbb899d3c6bee5fc9e52308c4199897bfae3065
grepoz <grzegorz.pozorski@gmail.com>
2023-07-28 23:22:56 +0200

    develop scraper & add simple gurufocus scraper

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import json
+import os
+import glob
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    directory = '../dataset/altman-z-score/'
+
+    tickers_json_file_path = 'scrapping/tickers.json'
+
+    with open(tickers_json_file_path, 'r') as file:
+        tickers = json.load(file)
+
+    found_tickers_json_file_path = 'tickers.json'
+
+    with open(found_tickers_json_file_path, 'r') as file:
+        found_tickers = json.load(file)
+
+    files = [f for f in glob.glob(f"{directory}*") if os.path.isfile(f)]
+
+    # Count the number of files
+    files_count = len(files)
+
+    not_found_tickers_json_file_path = 'scrapping/tickers.json'
+
+    with open(not_found_tickers_json_file_path, 'r') as file:
+        not_found_tickers = json.load(file)
+
+    print(f'Number of tickers: {len(tickers)}, not found tickers: {len(not_found_tickers)}')
--- scrapping/ycharts-scraper.py
+++ scrapping/ycharts-scraper.py
@@ -5,21 +5,30 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import pandas as pd
+import json
+import os
+import glob
+import time
+import datetime
+import tqdm
 
 login_url = "https://ycharts.com/login"
-scrape_url = "https://ycharts.com/companies/WMT/altman_z_score"
+# scrape_url = "https://ycharts.com/companies/WMT/altman_z_score"
 
 payload = {
     "username": "darekkruszel15@gmail.com",
-    "password": "set-env-var"
+    "password": "!Jaszczur15!"
 }
 
-df = pd.DataFrame(columns=['altman z-score'])
+df = pd.DataFrame(columns=['AltmanZScore'])
+not_found_tickers = []
 
 driver = webdriver.Chrome()
 
-try:
+cnt = 0
+files_count = 0
 
+try:
     driver.implicitly_wait(5)
 
     driver.get(login_url)
@@ -36,30 +45,84 @@ try:
 
     password.send_keys(Keys.RETURN)
 
-    print("Login successful")
+    # file things
+    directory = '../dataset/altman-z-score/'
 
-    driver.get(scrape_url)
-    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'tr')))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-    a = driver.page_source
+    json_file_path = 'tickers.json'
 
-    soup = BeautifulSoup(a, 'html.parser')
+    with open(json_file_path, 'r') as file:
+        tickers = json.load(file)
 
-    custom_element = soup.find('ycn-historical-data-table')
-    table = custom_element.find('div', {'class': 'row'})
+    files = [f for f in glob.glob(f"{directory}*") if os.path.isfile(f)]
 
-    for row in table.find_all('tr'):
-        row = [cell.text for cell in row.find_all('td')]
-        if len(row) == 0:
+    # Count the number of files
+    files_count = len(files)
+
+    json_file_path = 'not_found_tickers_2023-07-28_21-13-34.json'
+
+    with open(json_file_path, 'r') as file:
+        not_found_tickers = json.load(file)
+
+    beginning_index = files_count + len(not_found_tickers)
+
+    for ticker in tickers[beginning_index:]:
+        scrape_url = f"https://ycharts.com/companies/{ticker}/altman_z_score"
+
+        driver.get(scrape_url)
+
+        page_source = driver.page_source
+
+        soup = BeautifulSoup(page_source, 'html.parser')
+
+        h1_tag = soup.find('h1', string='Page Not Found')
+
+        if h1_tag or driver.current_url != scrape_url:
+            not_found_tickers.append(ticker)
             continue
-        date = pd.to_datetime(row[0])
-        score = float(row[1].strip())
+        else:
+            page_source = driver.page_source
 
-        df.loc[date] = score
+            WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'tr')))
 
-    print(df)
+            page_source = driver.page_source
+
+            soup = BeautifulSoup(page_source, 'html.parser')
+
+            custom_element = soup.find('ycn-historical-data-table')
+            table = custom_element.find('div', {'class': 'row'})
+
+            for row in table.find_all('tr'):
+                row = [cell.text for cell in row.find_all('td')]
+                if len(row) == 0:
+                    continue
+
+                try:
+                    date = pd.to_datetime(row[0])
+                except ValueError:
+                    date = None
+
+                try:
+                    score = float(row[1].strip())
+                except ValueError:
+                    score = None
+
+                df.loc[date] = score
+
+            df.index.name = 'DateTime'
+
+            df.to_csv(f'{directory}{ticker}.csv')
+
+            cnt += 1
+            time.sleep(1)
 
 except Exception as e:
+    print(f"Ticker counter: {cnt + files_count}")
     print(f"An error occurred: {e}")
 finally:
+    now_str = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    with open(f'not_found_tickers_{now_str}.json', 'w') as f:
+        json.dump(not_found_tickers, f)
     driver.quit()
