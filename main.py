857de1b657c9b54ccb95ea1f39c3f7bbaa9269ea
grepoz <grzegorz.pozorski@gmail.com>
2023-07-29 12:23:56 +0200

    refactor & add count results

--- main.py
+++ main.py
@@ -4,26 +4,24 @@ import glob
 
 if __name__ == '__main__':
 
-    directory = '../dataset/altman-z-score/'
+    directory = 'dataset/altman-z-score-incorrect/'
+
+    if not os.path.exists(directory):
+        print('directory does not exist')
 
     tickers_json_file_path = 'scrapping/tickers.json'
 
     with open(tickers_json_file_path, 'r') as file:
         tickers = json.load(file)
 
-    found_tickers_json_file_path = 'tickers.json'
-
-    with open(found_tickers_json_file_path, 'r') as file:
-        found_tickers = json.load(file)
-
     files = [f for f in glob.glob(f"{directory}*") if os.path.isfile(f)]
 
     # Count the number of files
     files_count = len(files)
 
-    not_found_tickers_json_file_path = 'scrapping/tickers.json'
+    not_found_tickers_json_file_path = 'scrapping/not_found_tickers_2023-07-28_23-29-05.json'
 
     with open(not_found_tickers_json_file_path, 'r') as file:
         not_found_tickers = json.load(file)
 
-    print(f'Number of tickers: {len(tickers)}, not found tickers: {len(not_found_tickers)}')
+    print(f'Number of tickers: {len(tickers)}, not found tickers: {len(not_found_tickers)}, files: {files_count}')
