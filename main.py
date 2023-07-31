ffb6602963bee370caf94fe3a3990fcb5d080b0c
grepoz <grzegorz.pozorski@gmail.com>
2023-07-31 22:02:42 +0200

    develop data analysis

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-import json
-import os
-import glob
-
 if __name__ == '__main__':
 
-    directory = 'dataset/altman-z-score-incorrect/'
-
-    if not os.path.exists(directory):
-        print('directory does not exist')
-
-    tickers_json_file_path = 'scrapping/qualified-tickers.json'
-
-    with open(tickers_json_file_path, 'r') as file:
-        tickers = json.load(file)
-
-    files = [f for f in glob.glob(f"{directory}*") if os.path.isfile(f)]
-
-    # Count the number of files
-    files_count = len(files)
-
-    not_found_tickers_json_file_path = 'scrapping/not_found_tickers_2023-07-29_11-32-36.json'
-
-    with open(not_found_tickers_json_file_path, 'r') as file:
-        not_found_tickers = json.load(file)
-
-    print(f'Number of tickers: {len(tickers)}, not found tickers: {len(not_found_tickers)}, files: {files_count}')
+    print('Running main.py...')
